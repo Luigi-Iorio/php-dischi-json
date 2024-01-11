8e7642006ca79a3215542c4409db0c3e1eea8966
Luigi-Iorio <luigi0iorio@gmail.com>
2024-01-11 15:47:45 +0100

    feat: salvataggio dati api in array dischi

--- js/script.js
+++ js/script.js
@@ -2,6 +2,18 @@ const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      dischi: [],
+    };
+  },
+  methods: {
+    getElencoAlbum() {
+      axios.get("server.php").then((response) => {
+        this.dischi = response.data;
+      });
+    },
+  },
+  created() {
+    this.getElencoAlbum();
   },
 }).mount("#app");
